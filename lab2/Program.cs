a6b6880ec158207ac01dc2fb02d99350933e1719
w70626 <w70626@RA117B-15>
2024-11-14 11:10:44 +0100

    Dodaj pliki projektów.

---
+++ lab2/Program.cs
@@ -0,0 +1,25 @@
+﻿using lab2.zadania;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person any = new Person("Name", "Surname", -15, "123456");
+        any.PrzedstawSie();  // Nazywam sie Name Surname i mam 0 lat
+
+        Quantity number = new Quantity(2);
+        number.Condition(); // 2
+        number.Sum(3); 
+        number.Condition(); // 5
+        number.Subtraction(1); 
+        number.Condition(); // 4
+
+        Adder nums = new Adder([1, 2, 3, 4]);
+        Console.WriteLine(nums.Sum()); // 10
+        Console.WriteLine(nums.SumDiv3()); // 2
+        Console.WriteLine(nums.Count()); // 4
+        nums.Display(0, 10); // 1 2 3 4
+
+    }
+}
+
---
+++ lab2/zadania/zadania.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2.zadania
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private int age;
+        private string pesel;
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age
+        {
+            get { return age; } 
+            set
+            {
+                if (value < 0)
+                {
+                    age = 0;
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public string Pesel { get; }
+
+        public Person(string name, string surname, int age, string pesel)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Pesel = pesel;
+        }
+
+        public void PrzedstawSie()
+        {
+            Console.WriteLine($"Nazywam się {Name} {Surname} i mam {Age} lat");
+        }
+    }
+
+    public class Quantity
+    {
+        private int Num { get; set; }
+
+        public Quantity(int num)
+        {
+            Num = num; 
+        }
+
+        public void Sum(int num)
+        {
+            Num += num;
+        }
+
+        public void Subtraction(int num)
+        {
+            Num -= num;
+        }
+
+        public void Condition()
+        {
+            Console.WriteLine(Num);
+        }
+    }
+
+    public class Adder(int[] num)
+    {
+        private int[] numbers = num;
+        public int Sum()
+        {
+            return numbers.Sum();
+        }
+        public double SumDiv3()
+        {
+            return (double)numbers.Sum(x => x/3);
+        }
+        public int Count()
+        {
+            return numbers.Count();
+        }
+        public void Display(int low, int high)
+        {
+            if(low < 0)
+            {
+                low = 0;
+            }
+            if(high > numbers.Length - 1)
+            {
+                high = numbers.Length - 1;
+            }
+            for (int i = low; i <= high; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+
+}
